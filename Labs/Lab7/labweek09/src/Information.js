5990e70efbd7959c568bb75d12076d8a48ec01c8
Amanda <amandagurns@hotmail.ca>
2024-11-01 09:06:49 -0400

    Pushing changes for week 9 lab 7.

---
+++ Labs/Lab7/labweek09/src/Information.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Information = () => {
+    return (
+        <div>
+            <h1>Welcome to Fullstack Development - I</h1>
+            <h2>React JS Programming Week09 Lab Exercise</h2>
+            <h3>101443253</h3>
+            <h4>Amanda Gurney</h4>
+            <h5>George Brown College, Toronto</h5>
+        </div>
+    );
+}
+
+export default Information;
